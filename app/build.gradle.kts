d14fbe86806793542469a7e3fdad81043129a676
zikassalvatore2-create <zikassalvatore2@gmail.com>
2026-04-26 19:58:04 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.vinotheque.nativeapp"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
 
     buildTypes {
