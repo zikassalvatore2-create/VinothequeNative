6df47639780b3ccd2253db05aa8bc859b0e63d5a
zikassalvatore2-create <zikassalvatore2@gmail.com>
2026-05-03 17:45:14 +0200

    Update SettingsScreen.kt

--- app/src/main/java/com/vinotheque/nativeapp/ui/SettingsScreen.kt
+++ app/src/main/java/com/vinotheque/nativeapp/ui/SettingsScreen.kt
@@ -5,6 +5,7 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
